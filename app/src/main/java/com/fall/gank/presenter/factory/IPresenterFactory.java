5cdec3031302fcf82bc7fe64702444bab13ef718
fallllllll <348476129@qq.com>
2017-01-18 17:23:40 +0800

    重构代码，增加对多Presenter的支持并写了一个多presenter的小测试

--- app/src/main/java/com/fall/gank/presenter/factory/IPresenterFactory.java
+++ app/src/main/java/com/fall/gank/presenter/factory/IPresenterFactory.java
@@ -7,7 +7,7 @@ import com.fall.gank.viewmodel.ITestModel;
  * Created by qqq34 on 2017/1/18.
  * 一个多presenter情况的例子。
  * 如果有一部分业务逻辑相同的话，那么我们可以把相同的部分抽离出来作为一个通用的presenter。
- * 为了方便以后的修改，通用的presenter需要用工厂来生成
+ * 因为通用的presenter会有很多界面使用，所以为了方便以后的修改，通用的presenter需要用工厂来生成
  */
 
 public interface IPresenterFactory {
